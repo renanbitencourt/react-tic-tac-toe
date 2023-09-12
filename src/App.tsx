40347df73733d8795e529c7dcd42722f6ba406b5
Renan de Bitencourt <renan.bitencourt@klir.com>
2023-09-12 20:39:28 -0300

    finishing game

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,86 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Board />
     </>
-  )
+  );
 }
 
 function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c] &&
+      squares[a] !== "-" &&
+      squares[b] !== "-" &&
+      squares[c] !== "-"
+    ) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
 
 function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill('-'));
+  const [squares, setSquares] = useState(Array(9).fill("-"));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
-    if (squares[i] !== '-') {
+    if (squares[i] !== "-" || calculateWinner(squares)) {
       return;
     }
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
+
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
 
+  function resetGame() {
+    setSquares(Array(9).fill("-"));
+    setXIsNext(true);
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -48,8 +96,9 @@ function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+      <button className="reset" onClick={resetGame}>Reset Game</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
